6ec483647dabfac08c13acfa463d57f69e140936
mahmud035 <mahamudulhasan4148@gmail.com>
2022-09-20 17:47:02 +0600

    pass gun id when clicked handleAddToCart()

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -4,20 +4,27 @@ import { BsFillCartFill } from 'react-icons/bs';
 
 const Card = (props) => {
   // console.log(props);
-  const { img, name, price, bullet, capacity, action } = props.gunData;
+  const { img, name, price, bullet, capacity, action, id } = props.gunData;
+
+  const handleAddToCart = (id) => {
+    console.log(id);
+  };
+
   return (
     <div className="card">
       <div className="image-container">
         <img src={img} alt="" />
       </div>
 
-      <h1>{name}</h1>
-      <p>Bullet Type: {bullet}</p>
-      <p>Capacity: {capacity}</p>
-      <p>Actions: {action}</p>
+      <div className="gun-info">
+        <h1>{name}</h1>
+        <p>Bullet Type: {bullet}</p>
+        <p>Capacity: {capacity}</p>
+        <p>Actions: {action}</p>
+      </div>
 
       <div className="add-to-cart">
-        <button>
+        <button onClick={() => handleAddToCart(id)}>
           <BsFillCartFill className="icon"></BsFillCartFill>
         </button>
         <h1> $ {price}</h1>
